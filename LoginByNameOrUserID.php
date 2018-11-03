d2589afae589c6750a60a0f2efe0aea15f3a6a47
Lerio <764109520@qq.com>
2018-11-03 12:57:58 +0800

    单页面登录根据用户名或者UID登录
    将LoginByNameOrUserID.php放到discuz网站根目录使用

--- LoginByNameOrUserID.php
+++ LoginByNameOrUserID.php
@@ -1,33 +1,24 @@
 <?php
-define('NOROBOT', FALSE);  
-define('ADMINSCRIPT', basename(__FILE__));  
-define('CURSCRIPT', 'admin');  
-define('HOOKTYPE', 'hookscript');  
-define('APPTYPEID', 0);  
-  
+/*请做好校验，推荐使用url加密验证*/
+
+$fxusername = $_GET['uname'];
 require_once './source/class/class_core.php';  
-  
 $discuz = C::app();  
+$discuz->init_mobile = false;
 $discuz->init();  
-  
-require_once libfile('function/member');  
-require_once libfile('class/member');  
-runhooks();  
 
 // 自动登录
-$fxusername = addslashes(trim($_GET['username'])); // 从url里获取的用户名
 $minfo = C::t('common_member')->fetch_by_username($fxusername);
 if(!empty($minfo)) {
     $uid = $minfo['uid'];
     $member = getuserbyuid($uid);
     if($member) {
-        loadcache('usergroups');
-        setloginstatus($member, 1296000);
+        require_once libfile('function/member');
+        setloginstatus($member, 2592000);
         DB::query("UPDATE ".DB::table('common_member_status')." SET lastip='".$_G['clientip']."', lastvisit='".TIMESTAMP."', lastactivity='".TIMESTAMP."' WHERE uid='$uid'");
-        loaducenter();
-        uc_api_post('user', 'synlogin', array('uid'=>$uid));
+        return true;
     }
 }
 
 //跳转到论坛首页
-header('Location:/DISCUZ');
+header('Location:./');
